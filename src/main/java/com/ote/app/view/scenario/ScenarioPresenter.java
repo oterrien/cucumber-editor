32ec057be97599aaa9a8fe8c12d568e9e717ce19
OTerrien <oterrien@neuf.fr>
2016-01-28 22:30:11 +0100

    update java

--- src/main/java/com/ote/app/view/scenario/ScenarioPresenter.java
+++ src/main/java/com/ote/app/view/scenario/ScenarioPresenter.java
@@ -20,6 +20,7 @@ public class ScenarioPresenter extends AbstractPresenter<ScenarioType, IScenario
         this.getView().setType(IScenarioView.Type.valueOf(model));
         this.getView().setTitle(model.getTitle());
         this.getView().setSteps(ScenarioStepsConverter.getInstance().getFormatter().format(model.getSteps()));
+		// System.out.println(model.toString());
     }
 
     @Override
